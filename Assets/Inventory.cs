d59d51bc272f4a312575305270e0d9434fd10b0d
Aleksandr Mikhaylov <funnybulletgames@yandex.ru>
2024-11-18 23:42:26 +0300

    Добавил возможность перетаскивания предметов из панели на поле

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -27,6 +27,8 @@ public class Inventory : MonoBehaviour
     public GameObject m_availableFurnitureObject;
     public List<ItemInventory> m_furnitureItems = new List<ItemInventory>();
 
+    public bool m_isAvailableFurniture = false;
+
     public void Start()
     {
         if (m_items.Count == 0)
@@ -98,11 +100,27 @@ public class Inventory : MonoBehaviour
 
     public void AddGraphics()
     {
+        for (int i = 0; i < m_data.m_items.Count; i++)
+        {
+            GameObject newItem = Instantiate(m_gameObjectShow, m_availableFurnitureObject.transform) as GameObject;
+            newItem.name = i.ToString() + "a";
+            ItemInventory ii = new ItemInventory();
+            ii.m_itemGameObject = newItem;
+            RectTransform rt = newItem.GetComponent<RectTransform>();
+            rt.localPosition = new Vector3(0, 0, 0);
+            rt.localScale = new Vector3(1, 1, 1);
+            newItem.GetComponentInChildren<RectTransform>().localScale = new Vector3(1, 1, 1);
+
+            Button tempButton = newItem.GetComponent<Button>();
+
+            tempButton.onClick.AddListener(delegate { SelectObject(); });
+            m_furnitureItems.Add(ii);
+        }
         for (int i = 0; i < m_maxCount; ++i)
         {
             GameObject newItem = Instantiate(m_gameObjectShow, m_inventoryMainObject.transform) as GameObject;
 
-            newItem.name = i.ToString();
+            newItem.name = i.ToString() + "i";
 
             ItemInventory ii = new ItemInventory();
             ii.m_itemGameObject = newItem;
@@ -115,23 +133,6 @@ public class Inventory : MonoBehaviour
 
             tempButton.onClick.AddListener(delegate { SelectObject(); });
             m_items.Add(ii);
-        }
-
-        for (int i = 0; i < m_data.m_items.Count; i++)
-        {
-            GameObject newItem = Instantiate(m_gameObjectShow, m_availableFurnitureObject.transform) as GameObject;
-            newItem.name = i.ToString();
-            ItemInventory ii = new ItemInventory();
-            ii.m_itemGameObject = newItem;
-            RectTransform rt = newItem.GetComponent<RectTransform>();
-            rt.localPosition = new Vector3(0, 0, 0);
-            rt.localScale = new Vector3(1, 1, 1);
-            newItem.GetComponentInChildren<RectTransform>().localScale = new Vector3(1, 1, 1);
-
-            Button tempButton = newItem.GetComponent<Button>();
-
-            tempButton.onClick.AddListener(delegate { SelectObject(); });
-            m_furnitureItems.Add(ii);
         }
     }
 
@@ -165,13 +166,12 @@ public class Inventory : MonoBehaviour
 
     }
 
-    // todo: переделать чтобы можно было брать из инвентаря
-    // нужно подумать над названиями
-    public void SelectObject()
+    // todo: смена когда меняешь в доступных местах
+    /*public void SelectObject()
     {
         if (m_currentId == -1)
         {
-            m_currentId = int.Parse(m_es.currentSelectedGameObject.name);
+            m_currentId = int.Parse(m_es.currentSelectedGameObject.name.Substring(0, m_es.currentSelectedGameObject.name.Length - 1));
             m_currentItem = CopyInventoryItem(m_items[m_currentId]);
             m_movingObject.gameObject.SetActive(true);
             m_movingObject.GetComponent<Image>().sprite = m_data.m_items[m_currentItem.m_id].m_img;
@@ -180,12 +180,59 @@ public class Inventory : MonoBehaviour
         }
         else
         {
-            AddInventoryItem(m_currentId, m_items[int.Parse(m_es.currentSelectedGameObject.name)]);
-            AddInventoryItem(int.Parse(m_es.currentSelectedGameObject.name), m_currentItem);
+            AddInventoryItem(m_currentId, m_items[int.Parse(m_es.currentSelectedGameObject.name.Substring(0, m_es.currentSelectedGameObject.name.Length - 1))]);
+            AddInventoryItem(int.Parse(m_es.currentSelectedGameObject.name.Substring(0, m_es.currentSelectedGameObject.name.Length - 1)), m_currentItem);
           
             m_currentId = -1;
 
             m_movingObject.gameObject.SetActive(false);
+        }
+    }*/
+    public void SelectObject()
+    {
+        if (m_currentId == -1)
+        {
+            if (m_es.currentSelectedGameObject.name[m_es.currentSelectedGameObject.name.Length - 1] == 'i')
+            {
+                m_currentId = int.Parse(m_es.currentSelectedGameObject.name.Substring(0, m_es.currentSelectedGameObject.name.Length - 1));
+                m_currentItem = CopyInventoryItem(m_items[m_currentId]);
+                m_movingObject.gameObject.SetActive(true);
+                m_movingObject.GetComponent<Image>().sprite = m_data.m_items[m_currentItem.m_id].m_img;
+
+                AddItem(m_currentId, m_data.m_items[0], 0);
+            }
+            else
+            {
+                m_currentId = int.Parse(m_es.currentSelectedGameObject.name.Substring(0, m_es.currentSelectedGameObject.name.Length - 1));
+                m_currentItem = CopyInventoryItem(m_furnitureItems[m_currentId]);
+                m_movingObject.gameObject.SetActive(true);
+                m_movingObject.GetComponent<Image>().sprite = m_data.m_items[m_currentItem.m_id].m_img;
+                m_isAvailableFurniture = true;
+            }
+        }
+        else
+        {
+            if (m_es.currentSelectedGameObject.name[m_es.currentSelectedGameObject.name.Length - 1] == 'a')
+            {
+                return;
+            }
+            if (m_isAvailableFurniture)
+            {
+                AddInventoryItem(int.Parse(m_es.currentSelectedGameObject.name.Substring(0, m_es.currentSelectedGameObject.name.Length - 1)), m_currentItem);
+                m_currentId = -1;
+
+                m_movingObject.gameObject.SetActive(false);
+                m_isAvailableFurniture = false;
+            }
+            else
+            {
+                AddInventoryItem(m_currentId, m_items[int.Parse(m_es.currentSelectedGameObject.name.Substring(0, m_es.currentSelectedGameObject.name.Length - 1))]);
+                AddInventoryItem(int.Parse(m_es.currentSelectedGameObject.name.Substring(0, m_es.currentSelectedGameObject.name.Length - 1)), m_currentItem);
+
+                m_currentId = -1;
+
+                m_movingObject.gameObject.SetActive(false);
+            }
         }
     }
 
